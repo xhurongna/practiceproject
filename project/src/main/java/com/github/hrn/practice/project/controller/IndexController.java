5d175576530e8f5b55d2989b93cefe92d92cb9b8
hurongna <159390@sh.lianjia.com>
2019-11-29 11:05:11 +0800

    imooc-http

--- project/src/main/java/com/github/hrn/practice/project/controller/IndexController.java
+++ project/src/main/java/com/github/hrn/practice/project/controller/IndexController.java
@@ -1,5 +1,6 @@
-package com.github.hrn.practice.project;
+package com.github.hrn.practice.project.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @author 胡荣娜
  * @date 2019-11-28
  */
+@Log4j2
 @Controller
 public class IndexController {
 
     @RequestMapping(value = "/index")
     @ResponseBody
     public String index() {
+        log.info("打印日志");
         return "你好";
     }
 }
