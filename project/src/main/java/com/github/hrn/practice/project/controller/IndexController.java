47cfe3e3ae9fb35103cc72cf05380f84f0525d50
hurongna <159390@sh.lianjia.com>
2019-12-01 17:47:25 +0800

    打印日志

--- project/src/main/java/com/github/hrn/practice/project/controller/IndexController.java
+++ project/src/main/java/com/github/hrn/practice/project/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.github.hrn.practice.project.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 胡荣娜
  * @date 2019-12-01
  */
+@Log4j2
 @RequestMapping("/index")
 @RestController
 public class IndexController {
 
     @ResponseBody
+    @RequestMapping(value = "/**")
     public String index() {
+        log.info("打印日志成功啦");
         return "你好";
     }
 }
