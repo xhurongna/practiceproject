c350957a0383901dd6051fe3dc4c186c7d67611e
hurongna <159390@sh.lianjia.com>
2019-12-01 17:10:19 +0800

    gitignore

--- demo/src/main/java/com/github/hrn/practice/demo/guava/eventbus/LogEventBus.java
+++ demo/src/main/java/com/github/hrn/practice/demo/guava/eventbus/LogEventBus.java
@@ -2,13 +2,12 @@ package com.github.hrn.practice.demo.guava.eventbus;
 
 import com.google.common.eventbus.Subscribe;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author 胡荣娜
  * @date 2019-05-14
  */
-@Slf4j
+@Log4j2
 public class LogEventBus {
     @Subscribe
     public void handleAllEvent(Object event) {
--- main/src/main/java/com/github/hrn/practice/main/MainApplication.java
+++ main/src/main/java/com/github/hrn/practice/main/MainApplication.java
@@ -1,12 +1,15 @@
 package com.github.hrn.practice.main;
 
+import com.github.hrn.practice.demo.guava.eventbus.LogEventBus;
 import com.google.common.eventbus.EventBus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,11 +34,11 @@ public class MainApplication {
         return eventBus;
     }
 
-//    @Bean(name = "taskPool")
-//    public TaskScheduler taskScheduler() {
-//        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-//        threadPoolTaskScheduler.setPoolSize(5);
-//        return threadPoolTaskScheduler;
-//    }
+    @Bean(name = "taskPool")
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(5);
+        return threadPoolTaskScheduler;
+    }
 
 }
