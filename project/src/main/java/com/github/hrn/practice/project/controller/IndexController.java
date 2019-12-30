7fe77df85fd8648cc6b855d570e84842f631a23a
hurongna <159390@sh.lianjia.com>
2019-12-30 13:05:51 +0800

    提交一波。。。大概是日志吧，忘记了

--- project/src/main/java/com/github/hrn/practice/project/controller/IndexController.java
+++ project/src/main/java/com/github/hrn/practice/project/controller/IndexController.java
@@ -1,14 +1,16 @@
 package com.github.hrn.practice.project.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 胡荣娜
- * @date 2019-12-01
+ * @date 2019-11-28
  */
+@Controller
 @Log4j2
 @RequestMapping("/index")
 @RestController
