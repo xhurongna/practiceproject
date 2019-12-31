fb080d0d89861050d6bf7cbab79418a0e6b1a4de
hurongna <159390@sh.lianjia.com>
2019-12-31 09:59:32 +0800

    Reduce

--- demo/src/main/java/com/github/hrn/practice/demo/lambda/Reduce.java
+++ demo/src/main/java/com/github/hrn/practice/demo/lambda/Reduce.java
@@ -14,5 +14,10 @@ public class Reduce {
         List<Tracks> tracks = Arrays.asList(new Tracks("Bakai", 524), new Tracks("Violets for you", 133), new Tracks("Time Was", 451));
         System.out.println(tracks.stream().reduce(BinaryOperator.minBy(Comparator.comparing(Tracks::getLength))));
         System.out.println(tracks.stream().reduce(BinaryOperator.maxBy(Comparator.comparing(Tracks::getLength))));
+
+        BinaryOperator<Integer> result = (acc, element) -> acc + element;
+//        System.out.println(Stream.of(1, 2, 3).reduce(0, result));
+        int count = result.apply(result.apply(result.apply(1, 0), 2), 3);
+        System.out.println(count);
     }
 }
